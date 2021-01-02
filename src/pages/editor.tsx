03657b0271df3fa73d41e4b3fab2ffff3ba05475
elletech <mygit_yutasugao20171126@yahoo.co.jp>
2021-01-02 01:30:13 +0000

    2-4 Web Storage を使って入力内容をブラウザに保存する

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,6 +1,11 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
+import { useStateWithStorage } from '../hooks/use_state_with_storage'
+
+const { useState } = React
+const StorageKey = 'pages/editor:text'
+
 const Header = styled.header`
     font-size: 1.5rem;
     height: 2rem;
@@ -44,13 +49,18 @@ const Preview = styled.div`
 `
 
 export const Editor: React.FC = () => {
+    const [text, setText] = useStateWithStorage('', StorageKey)
+
     return (
         <>
         <Header>
             Markdown Editor
         </Header>
         <Wrapper>
-            <TextArea value="テキスト入力エリア" />
+            <TextArea
+                onChange={(event) => setText(event.target.value)}
+                value={text}
+            />
             <Preview>プレビューエリア</Preview>
         </Wrapper>
         </>
